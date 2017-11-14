0969e0ae403f603b017755d3c59d66d5e6081379
Alvin <Alvin@example.com>
2017-11-14 05:38:49 +0000

    delete old files

--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    angular.module('portalApp', [
-        'ui.router',                    // Routing
-        'oc.lazyLoad',                  // LazyLoad Plugin
-    ])
-})();
-
-// Other libraries are loaded dynamically in the config.js
